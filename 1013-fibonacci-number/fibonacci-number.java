e9164bdad1d7da28d2cb70a1e90f647136d2344a
Rithiga V <138615122+rithigavijayendran@users.noreply.github.com>
2025-03-17 11:55:09 +0530

    Time: 100 ms (12.01%) | Memory: 40.7 MB (17.70%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,16 @@
 class Solution {
+    public int fibo(int n,int[] dp){
+        Arrays.fill(dp,-1);
+        if(n==0 || n==1) return n;
+        if(dp[n]!=-1) return dp[n];
+        int left = fibo(n-2,dp);
+        int right = fibo(n-1,dp);
+        dp[n] = left+right;
+        return dp[n];
+        
+    }
     public int fib(int n) {
-        if(n==0||n==1){
-            return n;
-        }
-        return fib(n-1)+fib(n-2);
+        int[] dp = new int[n+1];
+        return fibo(n,dp);
     }
 }
