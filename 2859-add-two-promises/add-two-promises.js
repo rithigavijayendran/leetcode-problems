30cfff2f0178c5338e374000a208d4dfdbf84b45
Rithiga V <138615122+rithigavijayendran@users.noreply.github.com>
2025-03-23 22:59:37 +0530

    Time: 49 ms (84.41%) | Memory: 53.7 MB (41.83%) - LeetSync

---
+++ 2859-add-two-promises/add-two-promises.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const sum = await promise1 + await promise2;
+    return sum;
+    };
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
